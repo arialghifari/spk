8a63e3240949908136025b6f743f137b8eb0bdd2
arialghifari <arialghifari99@gmail.com>
2022-11-16 23:16:20 +0800

    Initial commit

---
+++ Smart.php
@@ -0,0 +1,42 @@
+<?php
+
+class Smart
+{
+	public $kriteria = array();
+	public $alternatif = array();
+
+	function __construct()
+	{
+		// Data Kriteria dan Bobot
+		array_push($this->kriteria, array('C1', 'Dokumen Legalitas Pemohon', 30, true));
+		array_push($this->kriteria, array('C2', 'Surat Tanah Agunan', 50, true));
+		array_push($this->kriteria, array('C3', 'PBB Agunan', 40, true));
+		array_push($this->kriteria, array('C4', 'NPWP', 20, true));
+		array_push($this->kriteria, array('C5', 'Usaha', 40, true));
+		array_push($this->kriteria, array('C6', 'Penghasilan', 50, true));
+		array_push($this->kriteria, array('C7', 'Surat Keterangan Tidak Silang Sengketa', 20, true));
+
+		// Data Alternatif
+		array_push($this->alternatif, array('Lulu', 'A1', 100, 100, 100, 50, 50, 60, 50));
+		array_push($this->alternatif, array('Dhika', 'A2', 100, 100, 50, 25, 75, 100, 50));
+		array_push($this->alternatif, array('Asril', 'A3', 100, 50, 100, 25, 75, 80, 25));
+	}
+
+	function totalBobotKriteria()
+	{
+		$total = 0;
+
+		foreach ($this->kriteria as $kriteria) {
+			$total += $kriteria[2];
+		}
+
+		return $total;
+	}
+
+	function normalisasiBobot($bobot)
+	{
+		$normalisasi = $bobot / $this->totalBobotKriteria();
+
+		return round($normalisasi, 3);
+	}
+}
---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once './Smart.php';
+
+$smart = new Smart();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SPK PEMBERIAN KREDIT USAHA RAKYAT</title>
+</head>
+
+<body>
+	<!-- Kriteria dan Bobot -->
+	<h2>Kriteria dan Bobot</h2>
+	<table border="1">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Kode</th>
+				<th>Kriteria</th>
+				<th>Bobot</th>
+				<th>Normalisasi Bobot</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php $no = 1;
+			foreach ($smart->kriteria as $kriteria) { ?>
+				<tr>
+					<td><?= $no ?></td>
+					<td><?= $kriteria[0] ?></td>
+					<td><?= $kriteria[1] ?></td>
+					<td><?= $kriteria[2] ?></td>
+					<td><?= $smart->normalisasiBobot($kriteria[2]) ?></td>
+				</tr>
+			<?php $no++;
+			} ?>
+			<tr>
+				<td colspan="3">Total</td>
+				<td colspan="2"><?= $smart->totalBobotKriteria(); ?></td>
+			</tr>
+		</tbody>
+	</table>
+
+	<!-- Alternatif -->
+	<h2>Data Alternatif</h2>
+	<table border="1">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Nama</th>
+				<th>Kode</th>
+				<th>C1</th>
+				<th>C2</th>
+				<th>C3</th>
+				<th>C4</th>
+				<th>C5</th>
+				<th>C6</th>
+				<th>C7</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php $no = 1;
+			foreach ($smart->alternatif as $alternatif) { ?>
+				<tr>
+					<td><?= $no ?></td>
+					<td><?= $alternatif[0] ?></td>
+					<td><?= $alternatif[1] ?></td>
+					<td><?= $alternatif[2] ?></td>
+					<td><?= $alternatif[3] ?></td>
+					<td><?= $alternatif[4] ?></td>
+					<td><?= $alternatif[5] ?></td>
+					<td><?= $alternatif[6] ?></td>
+					<td><?= $alternatif[7] ?></td>
+					<td><?= $alternatif[8] ?></td>
+				</tr>
+			<?php $no++;
+			} ?>
+		</tbody>
+	</table>
+</body>
+
+</html>

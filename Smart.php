fc9424b0f94debca2fd7b648b780c3140f265a5c
arialghifari <arialghifari99@gmail.com>
2022-11-17 09:12:59 +0800

    Dynamic calculation

--- Smart.php
+++ Smart.php
@@ -18,15 +18,13 @@ class Smart
 		array_push($this->kriteria, array('C3', 'PBB Agunan', 40, true));
 		array_push($this->kriteria, array('C4', 'NPWP', 20, true));
 		array_push($this->kriteria, array('C5', 'Usaha', 40, true));
-		array_push($this->kriteria, array('C6', 'Penghasilan', 50, true));
-		array_push($this->kriteria, array('C7', 'Surat Keterangan Tidak Silang Sengketa', 20, true));
 
 		// Data Alternatif
-		array_push($this->alternatif, array('Lulu', 'A1', 100, 100, 100, 50, 50, 60, 50));
-		array_push($this->alternatif, array('Dhika', 'A2', 100, 100, 50, 25, 75, 100, 50));
-		array_push($this->alternatif, array('Asril', 'A3', 100, 50, 100, 25, 75, 80, 25));
-		array_push($this->alternatif, array('Demian', 'A4', 50, 50, 100, 50, 100, 40, 50));
-		array_push($this->alternatif, array('Ardhito', 'A5', 50, 100, 50, 25, 100, 20, 25));
+		array_push($this->alternatif, array('Lulu', 'A1', 100, 100, 100, 50, 50));
+		array_push($this->alternatif, array('Dhika', 'A2', 100, 100, 50, 25, 75));
+		array_push($this->alternatif, array('Asril', 'A3', 100, 50, 100, 25, 75));
+		array_push($this->alternatif, array('Demian', 'A4', 50, 50, 100, 50, 100));
+		array_push($this->alternatif, array('Ardhito', 'A5', 50, 100, 50, 25, 100));
 
 		$this->normalisasiBobot();
 		$this->cmin();
@@ -99,7 +97,11 @@ class Smart
 				if ($this->cmax[$i] - $this->cmin[$i] == 0) {
 					$alternatif[$i + 2] = 0;
 				} else {
-					$alternatif[$i + 2] = round(($alternatif[$i + 2] - $this->cmin[$i]) / ($this->cmax[$i] - $this->cmin[$i]), 3);
+					if ($alternatif[3]) {
+						$alternatif[$i + 2] = round(($alternatif[$i + 2] - $this->cmin[$i]) / ($this->cmax[$i] - $this->cmin[$i]), 3);
+					} else {
+						$alternatif[$i + 2] = round(($this->cmax[$i] - $alternatif[$i + 2]) / ($this->cmax[$i] - $this->cmin[$i]), 3);
+					}
 				}
 			}
 
--- index.php
+++ index.php
@@ -14,174 +14,179 @@ $smart = new Smart();
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>SPK PEMBERIAN KREDIT USAHA RAKYAT</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
 <body>
-	<!-- Kriteria dan Bobot -->
-	<h2>Kriteria dan Bobot</h2>
-	<table border="1">
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Kode</th>
-				<th>Kriteria</th>
-				<th>Bobot</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $no = 1;
-			foreach ($smart->kriteria as $kriteria) { ?>
+	<div class="container">
+		<!-- Kriteria dan Bobot -->
+		<h2>Kriteria dan Bobot</h2>
+		<table class="table">
+			<thead>
 				<tr>
-					<td><?= $no ?></td>
-					<td><?= $kriteria[0] ?></td>
-					<td><?= $kriteria[1] ?></td>
-					<td><?= $kriteria[2] ?></td>
+					<th>No</th>
+					<th>Kode</th>
+					<th>Kriteria</th>
+					<th>Bobot</th>
 				</tr>
-			<?php $no++;
-			} ?>
-			<tr>
-				<td colspan="3">Total</td>
-				<td><?= $smart->totalBobotKriteria(); ?></td>
-			</tr>
-		</tbody>
-	</table>
-
-	<!-- Alternatif -->
-	<h2>Data Alternatif</h2>
-	<table border="1">
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Nama</th>
-				<th>Kode</th>
-				<th>C1</th>
-				<th>C2</th>
-				<th>C3</th>
-				<th>C4</th>
-				<th>C5</th>
-				<th>C6</th>
-				<th>C7</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $no = 1;
-			foreach ($smart->alternatif as $alternatif) { ?>
+			</thead>
+			<tbody>
+				<?php $no = 1;
+				foreach ($smart->kriteria as $kriteria) { ?>
+					<tr>
+						<td><?= $no ?></td>
+						<td><?= $kriteria[0] ?></td>
+						<td><?= $kriteria[1] ?></td>
+						<td><?= $kriteria[2] ?></td>
+					</tr>
+				<?php $no++;
+				} ?>
 				<tr>
-					<td><?= $no ?></td>
-					<?php for ($i = 0; $i <= count($smart->cmin) + 1; $i++) { ?>
-						<td><?= $alternatif[$i] ?></td>
+					<td colspan="3">Total</td>
+					<td><?= $smart->totalBobotKriteria(); ?></td>
+				</tr>
+			</tbody>
+		</table>
+
+		<!-- Alternatif -->
+		<h2>Data Alternatif</h2>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Nama</th>
+					<th>Kode</th>
+					<th>C1</th>
+					<th>C2</th>
+					<th>C3</th>
+					<th>C4</th>
+					<th>C5</th>
+					<th>C6</th>
+					<th>C7</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $no = 1;
+				foreach ($smart->alternatif as $alternatif) { ?>
+					<tr>
+						<td><?= $no ?></td>
+						<?php for ($i = 0; $i <= count($smart->cmin) + 1; $i++) { ?>
+							<td><?= $alternatif[$i] ?></td>
+						<?php } ?>
+					</tr>
+				<?php $no++;
+				} ?>
+				<tr>
+					<td colspan="10"></td>
+				</tr>
+				<tr>
+					<td colspan="3">Cmin</td>
+					<?php foreach ($smart->cmin as $cmin) { ?>
+						<td><?= $cmin ?></td>
 					<?php } ?>
 				</tr>
-			<?php $no++;
-			} ?>
-			<tr>
-				<td colspan="10"></td>
-			</tr>
-			<tr>
-				<td colspan="3">Cmin</td>
-				<?php foreach ($smart->cmin as $cmin) { ?>
-					<td><?= $cmin ?></td>
-				<?php } ?>
-			</tr>
-			<tr>
-				<td colspan="3">Cmax</td>
-				<?php foreach ($smart->cmax as $cmax) { ?>
-					<td><?= $cmax ?></td>
-				<?php } ?>
-			</tr>
-		</tbody>
-	</table>
-
-	<!-- Normalisasi Bobot -->
-	<h2>Normalisasi Bobot</h2>
-	<table border="1">
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Kode</th>
-				<th>Kriteria</th>
-				<th>Bobot</th>
-				<th>Normalisasi Bobot</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $no = 1;
-			foreach ($smart->normalisasiBobot as $kriteria) { ?>
 				<tr>
-					<td><?= $no ?></td>
-					<td><?= $kriteria[0] ?></td>
-					<td><?= $kriteria[1] ?></td>
-					<td><?= $kriteria[2] ?></td>
-					<td><?= $kriteria[4] ?></td>
-				</tr>
-			<?php $no++;
-			} ?>
-		</tbody>
-	</table>
-
-	<!-- Nilai Utilitas -->
-	<h2>Nilai Utilitas</h2>
-	<table border="1">
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Kode</th>
-				<th>C1</th>
-				<th>C2</th>
-				<th>C3</th>
-				<th>C4</th>
-				<th>C5</th>
-				<th>C6</th>
-				<th>C7</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $no = 1;
-			foreach ($smart->utilitas as $utilitas) { ?>
-				<tr>
-					<td><?= $no ?></td>
-					<td><?= $utilitas[1] ?></td>
-					<?php for ($i = 2; $i <= count($smart->cmin) + 1; $i++) { ?>
-						<td><?= $utilitas[$i] ?></td>
+					<td colspan="3">Cmax</td>
+					<?php foreach ($smart->cmax as $cmax) { ?>
+						<td><?= $cmax ?></td>
 					<?php } ?>
 				</tr>
-			<?php $no++;
-			} ?>
-		</tbody>
-	</table>
+			</tbody>
+		</table>
 
-	<!-- Nilai Akhir -->
-	<h2>Nilai Akhir</h2>
-	<table border="1">
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Kode</th>
-				<th>C1</th>
-				<th>C2</th>
-				<th>C3</th>
-				<th>C4</th>
-				<th>C5</th>
-				<th>C6</th>
-				<th>C7</th>
-				<th>Total</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $no = 1;
-			foreach ($smart->nilaiAkhir as $nilaiAkhir) { ?>
-
+		<!-- Normalisasi Bobot -->
+		<h2>Normalisasi Bobot</h2>
+		<table class="table">
+			<thead>
 				<tr>
-					<td><?= $no ?></td>
-					<td><?= $nilaiAkhir[1] ?></td>
-					<?php for ($i = 1; $i <= count($smart->cmin) + 1; $i++) { ?>
-						<td><?= $nilaiAkhir[$i + 1] ?></td>
-					<?php } ?>
+					<th>No</th>
+					<th>Kode</th>
+					<th>Kriteria</th>
+					<th>Bobot</th>
+					<th>Normalisasi Bobot</th>
 				</tr>
-			<?php $no++;
-			} ?>
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				<?php $no = 1;
+				foreach ($smart->normalisasiBobot as $kriteria) { ?>
+					<tr>
+						<td><?= $no ?></td>
+						<td><?= $kriteria[0] ?></td>
+						<td><?= $kriteria[1] ?></td>
+						<td><?= $kriteria[2] ?></td>
+						<td><?= $kriteria[4] ?></td>
+					</tr>
+				<?php $no++;
+				} ?>
+			</tbody>
+		</table>
+
+		<!-- Nilai Utilitas -->
+		<h2>Nilai Utilitas</h2>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Kode</th>
+					<th>C1</th>
+					<th>C2</th>
+					<th>C3</th>
+					<th>C4</th>
+					<th>C5</th>
+					<th>C6</th>
+					<th>C7</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $no = 1;
+				foreach ($smart->utilitas as $utilitas) { ?>
+					<tr>
+						<td><?= $no ?></td>
+						<td><?= $utilitas[1] ?></td>
+						<?php for ($i = 2; $i <= count($smart->cmin) + 1; $i++) { ?>
+							<td><?= $utilitas[$i] ?></td>
+						<?php } ?>
+					</tr>
+				<?php $no++;
+				} ?>
+			</tbody>
+		</table>
+
+		<!-- Nilai Akhir -->
+		<h2>Nilai Akhir</h2>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Kode</th>
+					<th>C1</th>
+					<th>C2</th>
+					<th>C3</th>
+					<th>C4</th>
+					<th>C5</th>
+					<th>C6</th>
+					<th>C7</th>
+					<th>Total</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $no = 1;
+				foreach ($smart->nilaiAkhir as $nilaiAkhir) { ?>
+
+					<tr>
+						<td><?= $no ?></td>
+						<td><?= $nilaiAkhir[1] ?></td>
+						<?php for ($i = 1; $i <= count($smart->cmin) + 1; $i++) { ?>
+							<td><?= $nilaiAkhir[$i + 1] ?></td>
+						<?php } ?>
+					</tr>
+				<?php $no++;
+				} ?>
+			</tbody>
+		</table>
+	</div>
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
 </html>
